ad1c060c0a3cce94b44a59a4c66c6095f09b9d9a
Sergio Isla <122633667+sergioisla@users.noreply.github.com>
2024-07-22 17:56:26 -0600

    Add files via upload

---
+++ movility.py
@@ -0,0 +1,27 @@
+import requests
+import pandas as pd
+
+cookies = {
+}
+
+headers = {
+    'cache-control': 'no-cache',
+    'content-type': 'application/json',
+    'origin': 'https://app.semovi.cdmx.gob.mx',
+    'pragma': 'no-cache',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+}
+
+json_data = {
+    'serie': 'C32F5537',
+    'anio': '2024',
+    'operacion': 'todas',
+}
+
+response = requests.post(
+    'https://app.semovi.cdmx.gob.mx/micrositio/291-trazabilidad_tarjetas.php',
+    headers=headers,
+    json=json_data,
+)
+df = pd.DataFrame(response.json()["data"])
+print(df.head())
