43b675f9ff061ee57b39f928fa4bcf9a76dc029d
sergio isla <lunespostal@gmail.com>
2024-07-23 16:54:59 -0600

    add 1 plot

--- movility.py
+++ movility.py
@@ -25,24 +25,19 @@ st.markdown(margins_css, unsafe_allow_html=True)
 
 #Title
 st.markdown("""
-    ## 🇲🇽 Mexico City - Safety Map
-    ### Don't Become a Statistic""")
+    ## 🇲🇽 Mexico City
+    ### Do Become a Statistic""")
 
 col1, col2 = st.columns([1,1])
 
 with col1:
     ## Safety Map Front
     st.markdown("""
-    Despite our love for CDMX, we cannot ignore the fact that it is not the safest place on earth.\n
-    However, we are here to empower you with amazing knowledge and provide a safety map for this incredible city.\n
-    Our aim is to offer a comprehensive overview of neighborhoods where you can enjoy tacos and mezcal without worries,
-    as well as areas where caution is advised.""")
+    Despite """)
 
 with col2:
     st.markdown("""
-    Our data encompasses all registered crimes in the Colonias of CDMX from **2019 to 2023**. By sharing this information, we hope to help you make informed decisions and navigate the city with confidence.\n
-    Remember, being aware is the first step towards safety. Let us guide you through the vibrant streets of CDMX, ensuring that you won't become just another statistic.\n
-    To be transparent from the beginning, you can find the data for our project publicly available [here](https://datos.cdmx.gob.mx/dataset/victimas-en-carpetas-de-investigacion-fgj#:~:text=Descargar-,V%C3%ADctimas%20en%20Carpetas%20de%20Investigaci%C3%B3n%20(completa),-CSV)
+    Our data )
     """)
 
 
@@ -86,6 +81,10 @@ df['mes'] = df['fecha'].dt.month
 df_recarga = df.loc[df['operacion'] == "00-RECARGA"]
 df_validacion = df.loc[df['operacion'] == "03-VALIDACION"]
 
+count_stns = pd.DataFrame(df_validacion.value_counts('estacion'))
+count_stns.reset_index(inplace=True)
+count_stns.columns = ['estacion', 'counts']
+
 total_recargas = df_recarga['monto'].sum()
 
 st.markdown("""
@@ -94,9 +93,9 @@ Total de recargas en el año:""")
 st.markdown(total_recargas)
 # Display the plot in Streamlit
 
-#altair_chart = alt.Chart(df_validacion).mark_bar().encode(
-    #x='estacion',
-    #y='b'
-    #)
+altair_chart = alt.Chart(count_stns).mark_bar().encode(
+    x='estacion',
+    y='counts'
+    )
 
-#st.altair_chart(altair_chart, use_container_width=False, theme="streamlit", key=None, on_select="ignore", selection_mode=None)
+st.altair_chart(altair_chart)
