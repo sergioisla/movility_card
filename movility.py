1c7c34d8b9cddfaa8fac3cad6e33b87a6ca9c8be
sergio isla <lunespostal@gmail.com>
2024-07-24 19:32:24 -0600

    title correction

--- movility.py
+++ movility.py
@@ -26,7 +26,8 @@ st.markdown(margins_css, unsafe_allow_html=True)
 #Title
 st.markdown("""
     ##
-    ### Aquí puedes ver un resumen de la información de tu tarjeta de movilidad de la CDMX por año""")
+    ### Tarjeta de movilidad de la CDMX
+    #### Aquí puedes ver un resumen de tus viajes por año""")
 #Option to write something in a pair of columns
 col1, col2 = st.columns([1,1])
 
